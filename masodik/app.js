8c2aa43a3cc5f1be435bdf7009d45767c4a34386
zamboilles21 <zamboilles@turr.hu>
2022-01-27 13:35:46 +0100

    aa

--- masodik/app.js
+++ masodik/app.js
@@ -1,1 +1,9 @@
 let box1 = document.getElementById('box1');
+let boxes = document.getElementsByClassName('box3');
+let boxType=document.getElementsByName('doboz');
+box1.innerHTML+=<p class="bekezdés">"Bekezdés"</p>;
+
+boxType.forEach(box3=>{box3.style.border='5px solid green';});
+
+let dobozok = document.querySelectorAll('box1');
+box1.style.left='100px'
