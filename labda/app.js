da3ad39092c383fee7107adfa3d520761d0c2823
zamboilles21 <zamboilles@turr.hu>
2022-01-28 11:16:20 +0100

    app

--- labda/app.js
+++ labda/app.js
@@ -1,14 +1,37 @@
 let ball = document.querySelector('#ball');
 let ballSize=54;
-let speed=1;
+let speed=3;
 let ballX=Math.round(Math.random()*screenX);
 let ballY=0;
-let screenX=window.innerWidth;
-let screenY=window.innerHeight;
-
+let screenX=window.innerWidth-ballSize;
+let screenY=window.innerHeight-ballSize;
+let dx=1;
+let dy=1;
 ball.style.top=ballY+'px';
 ball.style.left=ballX+'px';
 
+ball.style.width=ballY+'px';
+ball.style.height=ballX+'px';
+
+setInterval(moveBall,10)
+
 function moveBall(){
-    
+    ballX+=dx*speed;
+    ballY+=dy*speed;
+    ball.style.top=ballY+'px';
+    ball.style.left=ballX+'px';
+    if((ballY>=screenY-6)||(ballY<=-8))
+    {
+        dy *= -1;
+    }
+    if((ballX>=screenX-6)||(ballX<=-8))
+    {
+        dx *= -1;
+
+    }
+}
+
+function setBallsize(size){
+    ball.style.width=size+'px';
+    ball.style.height=size+'px';
 }
