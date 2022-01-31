d116c26c1cb6f76a62b004c56429cdf295e84df4
zamboilles21 <zamboilles@turr.hu>
2022-01-31 08:35:44 +0100

    labda2

--- labda/app.js
+++ labda/app.js
@@ -1,8 +1,10 @@
 let ball = document.querySelector('#ball');
 let bat = document.querySelector('#bat');
+let pointsLbl = document.querySelector('#pointsLbl');
+let speedLbl=document.querySelector('#speedLbl');
 let ballSize = 54;
-let batSize = 122;
-let speed = 3;
+let batSize = 120;
+let speed = 2;
 let screenX;
 let screenY;
 let ballX;
@@ -11,17 +13,15 @@ let batX;
 let batY;
 let dx = 1;
 let dy = 1;
+let points=0;
 
 getWindowSize();
-
 setBallStartPoz();
-setBatStartPoz();
-setBatPoz();
-
+setBatStartPoz()
 setBallSize(ballSize);
 setBatSize(batSize);
 
-setInterval(movingBall, 10);
+let game = setInterval(movingBall, 10);
 
 // mozgatjuk a labdát
 function movingBall(){
@@ -31,25 +31,33 @@ function movingBall(){
     setBallPoz(ballX, ballY); 
 
     // ütközés a képernyő szélével
-    if ((ballY >= screenY-8) || (ballY <= -8))
+    if (ballY <= -8)
     {
         dy *= -1;
+        points++;
+        updateResults();
     }
     
     if ((ballX >= screenX-8) || (ballX <= -8))
     {
         dx *= -1;
     }
-    if (ballY>=batY) {
-        let ballMid=(ballX+ballSize)/2;
-        if ((ballMid>=batX)&&(ballMid<=(batX+ballSize))) {
-            dy*=-1
+
+    // ha a labda eléri az ütő vonalát, megnézzük, hogy alatta van-e az ütő
+    if (ballY >= batY){
+        let ballMid = ballX + (ballSize) / 2;
+
+        if ((ballMid >= batX) && (ballMid <= (batX+batSize)))
+        {
+            dy *= -1;
         }
-        clearInterval(game);
     }
-    if(ballY>=screenY-8){
+
+    // ha a labda leesett
+    if (ballY >= screenY-8)
+    {
         clearInterval(game);
-        window.alert('GAME OVER')
+        window.alert('GAME OVER!');
     }
 }
 
@@ -58,9 +66,11 @@ function setBallSize(size){
     ball.style.width = size+'px';
     ball.style.height = size+'px';
 }
+
+// beállítjuk az ütő méretét
 function setBatSize(size){
     bat.style.width = size+'px';
-    bat.style.height = 15 +'px';
+    bat.style.height = '15px';
 }
 
 // beállítjuk a labda pozícióját
@@ -68,8 +78,11 @@ function setBallPoz(x, y){
     ball.style.top = y + 'px';
     ball.style.left = x + 'px'; 
 }
-function setBatPoz(X){
-    bat.style.left = batY + 'px'; 
+
+// beállítjuk az ütő pozícióját
+function setBatPoz(x){
+    bat.style.top = batY + 'px';
+    bat.style.left = x + 'px'; 
 }
 
 // beállítjuk a labda kezdőpozícióját
@@ -78,10 +91,12 @@ function setBallStartPoz(){
     ballY = 0;
     setBallPoz(ballX, ballY); 
 }
+
+// beállítjuk az ütő kezdőpozícióját
 function setBatStartPoz(){
     batX = 0;
     batY = screenY-40;
-    setBallPoz(batX, batY); 
+    setBatPoz(batX); 
 }
 
 // lekérdezzük az aktuális képernyő méretet
@@ -91,27 +106,18 @@ function getWindowSize(){
     setBatStartPoz();
 }
 
-window.addEventListener('resize',getWindowSize);
+// az ablak átméretezésekor frissítjük a képernyő koordinálátkat
+window.addEventListener('resize', getWindowSize);
 
-window.addEventListener('keydown',function(e){
-    if(e.keyCode==39)
-    {
-        if(batX+batSize<screenX)
-        {
-            batX+=15;
-            setBatPoz(batX,screenY-45);
-        }
-    }
-    if(e.keyCode==37)
-    {
-        if(batX+batSize>0)
-        {
-            batX-=15;
-            setBatPoz(batX,screenY-45);
-        }
-    }
-});
-window.addEventListener('mousemove',function(e){
-    batX=e.x;
-    setBatPoz(batX)
+// mozgatjuk az ütőt a nyilakkal
+document.addEventListener('mousemove', function(e){
+    batX = e.x;
+    console.log(batX);
+    setBatPoz(batX);
+
 })
+
+function updateResults(){
+    pointsLbl.innerHTML=points;
+    speedLbl.innerHTML=speed;
+}
