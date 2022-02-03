072daf98e77e909e3f608a94fb4156ff8a0822f2
zamboilles21 <zamboilles@turr.hu>
2022-02-03 13:55:46 +0100

    commit

---
+++ orarend/app.js
@@ -0,0 +1,14 @@
+let napok=['Hétfő','Kedd','Szerda','Csütötök','Péntek'];
+let orak=[0,2,3,4,5,6,7,8];
+let tantargyal=[];
+
+let daySelect=document.querySelector('#day');
+let hourSelect=document.querySelector('#hour');
+let subjectSelect=document.querySelector('#subjects');
+let addBtn=document.querySelector('#addBtn');
+
+addDays();
+
+function addDays() {
+    
+}
