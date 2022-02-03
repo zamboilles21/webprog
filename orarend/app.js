1328bf09c5e9bc427907317ef0aefcdafb7fb76a
zamboilles21 <zamboilles@turr.hu>
2022-02-03 14:07:46 +0100

    valtozas

--- orarend/app.js
+++ orarend/app.js
@@ -1,6 +1,10 @@
 let napok=['Hétfő','Kedd','Szerda','Csütötök','Péntek'];
 let orak=[0,2,3,4,5,6,7,8];
-let tantargyal=[];
+let tantargyak=[
+    {
+        
+    }
+];
 
 let daySelect=document.querySelector('#day');
 let hourSelect=document.querySelector('#hour');
@@ -8,7 +12,36 @@ let subjectSelect=document.querySelector('#subjects');
 let addBtn=document.querySelector('#addBtn');
 
 addDays();
+addHours();
+addSubjects();
 
 function addDays() {
-    
+    let i = 0;
+    napok.forEach(nap=>{
+        let day=document.createElement('option');
+        day.value=i;
+        day.innerHTML=nap;
+        daySelect.appendChild(day);
+        i++;
+    });
+}
+function addHours() {
+    let i = 0;
+    napok.forEach(ora=>{
+        let hour=document.createElement('option');
+        ora.value=i;
+        ora.innerHTML=nap;
+        hourSelect.appendChild(ora);
+        i++;
+    });
+}
+function addSubjects() {
+    let i = 0;
+    napok.forEach(tantargy=>{
+        let subject=document.createElement('option');
+        subject.value=i;
+        subject.innerHTML=tantargy;
+        subkectSelect.appendChild(subject);
+        i++;
+    });
 }
