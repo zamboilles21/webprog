50826fd3861d3a9535157aa0f72de1176869d968
zamboilles21 <zamboilles@turr.hu>
2022-02-10 13:25:58 +0100

    mukdoik

--- Husvet/app.js
+++ Husvet/app.js
@@ -1,21 +1,26 @@
-let szam=document.querySelector('#szam');
-let addBtn=document.querySelector('#gomb');
-let T;
 
+let addBtn=document.querySelector('#gomb');
 
 addBtn.addEventListener('click',function() {
+    let szam=document.querySelector('#szam').value;
+
+let T;
+    T=szam;
     if (1901<=T<=2099) {
-        T=szam;
+        
         let A=T%19;
         let B=T%4;
         let C=T%7;
         let D=(19*A+24)%30;
         let E=(2*B+4*C+6*D+5)%7;
         let H=22+D+E;
-    }
-    if (T>31) {
+        if (T>31) {
         
+        }
+        console.log(A);
     }
+    
+    
 })
 
 
