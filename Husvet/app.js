cb6a7db91786e779370384dce4a8c19c80dfc15d
zamboilles21 <zamboilles@turr.hu>
2022-02-10 13:18:02 +0100

    ures

--- Husvet/app.js
+++ Husvet/app.js
@@ -1,14 +1,22 @@
 let szam=document.querySelector('#szam');
 let addBtn=document.querySelector('#gomb');
 let T;
-let A=T/19;
-let B=T/4;
-let C=T/7;
-let D=(19*A+24)/30;
-let E=(2*B+4*C+6*D+5)/7;
-let H=22+D+E;
 
 
+addBtn.addEventListener('click',function() {
+    if (1901<=T<=2099) {
+        T=szam;
+        let A=T%19;
+        let B=T%4;
+        let C=T%7;
+        let D=(19*A+24)%30;
+        let E=(2*B+4*C+6*D+5)%7;
+        let H=22+D+E;
+    }
+    if (T>31) {
+        
+    }
+})
 
 
 
