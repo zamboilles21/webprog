d74abbd8400fe881c6f9f547d3204796f02969d1
zamboilles21 <zamboilles@turr.hu>
2022-02-17 13:24:35 +0100

    1

--- színház/app.js
+++ színház/app.js
@@ -22,3 +22,44 @@ hely = new Array()
 		document.write("<br>")
 		if(i%5==0) document.write("<hr size=1 color=#880000>")
 	}
+
+    //1.feladat
+    let szabadhelyekSzama=0;
+    for(i=1;i<=15;i++){
+        for(j=1;j<=24;j++){
+            if (szabadhely(i,j)==0) {
+                szabadhelyekSzama++;
+            }
+        }
+    }
+
+    document.writeln("1.feladat: A szabad helyek száma: "+szabadhelyekSzama);
+
+    function szabadhely(x,y) {
+        return hely[x][y];
+    }
+
+    //2.feladat
+    document.writeln('<br>')
+    document.writeln("2.feladat: Eladott jegyek százaléka:  "+(((15*24)-szabadhelyekSzama)/(15*24))*100+"%");
+
+    //3.feladat
+    let jegyarbevetel=0;
+
+    for(i=1;i<=15;i++){
+        for (let i =0; i <= 24; i++){
+            if (hely[i][j]==1) {
+                if (i<6) {
+                    jegyarbevetel+=7500;
+                }
+                if (i>=6 && i<11) {
+                    jegyarbevetel+=5900;
+                }
+                if (i>=11) {
+                    jegyarbevetel+=4500;
+                }
+            }
+           
+        }
+    }
+    document.writeln(`<br>3.feladat: Jegyárbevétel: ${jegyarbevetel} ft`);
