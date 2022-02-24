7a72ab17be89437a79e705a60c16bee2d2d74613
zamboilles21 <zamboilles@turr.hu>
2022-02-24 13:41:02 +0100

    aaa

--- edzesnaptar/app.js
+++ edzesnaptar/app.js
@@ -6,23 +6,40 @@ let ma=new Date();
 let aktev=ma.getFullYear();
 let napszamok=[31,28+szokoev(aktev),31,30,31,30,31,31,30,31,30,31]
 let  aktho=ma.getUTCMonth();
-let aktnap=ma.getUTCDay();
-
+let aktnap=ma.getDate();
+let table=document.querySelector("#edzesnaptar");
 console.log(aktho)
 console.log(aktnap)
 console.log(aktev)
 
 
-let k=0;
-for (let i = 0; i < 30; i++) {
-    document.writeln(kiir(aktho+1)+'.'+kiir(k)+'<br>');
+let k=aktnap;
+for (i = 0; i < 30; i++) {
+    naptarhoz_ad(aktho,k);
     k++;
-    if ((aktnap+k)>napszamok[aktho]) {
+    if (k>napszamok[aktho]) {
         aktho++;
         k=1;
     }
     
+
 }
+
+function naptarhoz_ad(aktho,nap)
+{
+    document.createElement('tr');
+    table.appendChild(tr);
+    let td1=document.createElement('td');
+    td1.classList('cella');
+    td1.innerHTML=kiir(aktho+1)+'.'+kiir(nap);
+    let td2=document.createElement('td');
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+
+}
+
+
 function kiir(szam) {
     if (szam<10) {
         szam='0'+szam;
