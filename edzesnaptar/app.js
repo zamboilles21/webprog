761944eea1effcefda688a9559e17adeff4ee8ae
zamboilles21 <zamboilles@turr.hu>
2022-02-24 13:17:16 +0100

    gyuras

--- edzesnaptar/app.js
+++ edzesnaptar/app.js
@@ -1,5 +1,41 @@
-var d = new Date();
-let year = d.getFullYear();
-let month = d.getMonth();
-let day = d.getDay();
+
+let edzesek=['váll- és házedzés','Karedzés','melledzés','lábedzés']
+let pihenonap='---';
+let aktedzes=3;
+let ma=new Date();
+let aktev=ma.getFullYear();
+let napszamok=[31,28+szokoev(aktev),31,30,31,30,31,31,30,31,30,31]
+let  aktho=ma.getUTCMonth();
+let aktnap=ma.getUTCDay();
+
+console.log(aktho)
+console.log(aktnap)
+console.log(aktev)
+
+
+let k=0;
+for (let i = 0; i < 30; i++) {
+    document.writeln(kiir(aktho+1)+'.'+kiir(k)+'<br>');
+    k++;
+    if ((aktnap+k)>napszamok[aktho]) {
+        aktho++;
+        k=1;
+    }
+    
+}
+function kiir(szam) {
+    if (szam<10) {
+        szam='0'+szam;
+    }
+    return szam;
+}
+function szokoev(ev) {
+    let nap=0;
+    if ((ev%4==0)&&(ev%100!=0)||(ev%400==0)) {
+        nap=1
+    }
+    return nap;
+}
+
+
 document.getElementById("datum").innerHTML = d;
