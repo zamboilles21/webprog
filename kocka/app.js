93322e07c9e31523fd075e59f07c764b5abbc4d5
zamboilles21 <zamboilles@turr.hu>
2022-02-25 11:19:55 +0100

    feltolt

---
+++ kocka/app.js
@@ -0,0 +1,15 @@
+let A=[];
+let B=[];
+let C=[];
+
+function feltolt()
+{
+    for (i = 0; i < 20; i++) 
+    {
+        A.push(Math.floor(Math.random() * 6));
+        B.push(Math.floor(Math.random() * 6));
+        C.push(Math.floor(Math.random() * 6));
+        
+    }
+
+}
