3b50cdff02478e7df10c3f18e2e48bc911d18187
zamboilles21 <zamboilles@turr.hu>
2022-02-27 21:47:48 +0100

    szar

--- kocka/app.js
+++ kocka/app.js
@@ -9,30 +9,67 @@ for (i = 0; i < 20; i++) {
     eredmenykiiras(i);
     
 }
-function eredmeny(x)
+function eredmenykiiras(x)
 {
     let div=document.createElement('div');
     container.appendChild(div);
     div.classList.add('dobas');
 
     let img=document.createElement('img');
-    img.src=`/img/${A[x]}.PNG`;
+    img.src=`${A[x]}.png`;
     img.title=`a[${x}]=${A[x]}`;
     div.appendChild(img);
     
-    let img=document.createElement('img');
-    img.src=`/img/${B[x]}.PNG`;
+    img=document.createElement('img');
+    img.src=`${B[x]}.png`;
     img.title=`B[${x}]=${B[x]}`;
     div.appendChild(img);
 
-    let img=document.createElement('img');
-    img.src=`/img/${C[x]}.PNG`;
+    img=document.createElement('img');
+    img.src=`${C[x]}.png`;
     img.title=`C[${x}]=${C[x]}`;
     div.appendChild(img);
 }
 
-function kockaDobas(x) {
+function kockadobas(x) {
     A[x]= (Math.round(Math.random()*5)+1);
     B[x]= (Math.round(Math.random()*5)+1);
     C[x]= (Math.round(Math.random()*5)+1);
 }
+function osszes() {
+    let osszeg = 0;
+    for(i = 0; i < A.length; i++){
+        osszeg += A[i] + B[i] + C[i];
+    }
+    return osszeg / A.length;
+    
+}
+document.writeln("Az összeg: "+osszes())
+console.log("Az összeg: "+osszes())
+function max()
+{
+    let max1 = 0;
+    for (i = 0; i < A.Length; i++)
+    {
+        if (A[i] + B[i] + c[i]> max1)
+        {
+            max1 = tomb[i];
+        }
+        return max1;
+    }
+    
+}
+document.writeln("MAX:"+max())
+console.log("MAX: "+max())
+function hatos(){
+    hatso=0;
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] == 6 & B[i]==6 & C[i] == 6) {
+            hatso++;
+        }
+        
+    }
+    return hatso;
+}
+document.writeln("Hatos: "+hatos())
+console.log("Hatos: "+max())
