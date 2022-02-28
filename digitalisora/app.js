07fb10da1ca60c2058786e3d3eb9da0671f02fb0
zamboilles21 <zamboilles@turr.hu>
2022-02-28 09:01:23 +0100

    jaja

--- digitalisora/app.js
+++ digitalisora/app.js
@@ -1,5 +1,12 @@
 let startstopBtn=document.querySelector('#startstop');
 let resetBtn=document.querySelector('#resetBtn');
+let kijelzo=document.querySelector('#kijelzo');
+let ezredmp=0, 
+    mp=0, 
+    perc=0,
+    ora=0
+
+reset();
 
 startstopBtn.addEventListener('click',function()
 {
@@ -10,3 +17,21 @@ resetBtn.addEventListener('click',function(){
 
 
 });
+
+function reset()
+{
+    ezredmp=0;
+    mp=0;
+    perc=0;
+    ora=0;
+
+    kijelzo.innerHTML=kiir(ora,2)+":"+kiir(perc,2)+":"+kiir(mp,2)+"."+kiir(ezredmp,3)
+}
+
+function kiir(mit, helyiertek)
+{
+    if (mit<10) {
+        mit='0'+mit
+    }
+    return mit;
+}
