7cd327a996abe4e8d7650dd93e59f427b9dc681c
zamboilles21 <zamboilles@turr.hu>
2022-04-28 12:29:19 +0200

    sststst

--- Urlapok2/js/login.js
+++ Urlapok2/js/login.js
@@ -1,3 +1,6 @@
+
+
+
 let regBtn = document.querySelector('#regBtn'),
     email = document.querySelector('#email'),
     pass = document.querySelector('#pass1');
@@ -11,16 +14,27 @@ if (users = localStorage.getItem('users')) {
 
 regBtn.addEventListener('click',()=>{
     let correct= false;
+    let name='';
     if (email.value!=""||pass.value!="") {
         users.forEach(user => {
             if (user.password==pass.value&&user.email==email.value){
                 correct=true;
+                name=user.name;
             }
         });
         if(correct){
             email.classList.remove('missing')
             pass.classList.remove('missing')
             alert('Sikeres bejelnetkezés!')
+            let data={
+                    'loggedIn':true,
+                    'loggedUser':name,
+                    'loggedUserMail':email.value
+
+
+            }
+            sessionStorage.setItem('access',JSON.stringify(data));
+            document.location.href='content.html'
         }
         else{
             alert('Hibás felhasználó név vagy jelszó')
