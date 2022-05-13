5ca91e45dbe3b0ec8583c0819c0cfdea77943350
zamboilles21 <zamboilles@turr.hu>
2022-05-13 09:40:03 +0200

    ghhhf

---
+++ jquery/weboldal1/js/app.js
@@ -0,0 +1,18 @@
+function contentLoad(page){
+    $('#content').load(page+'.html');
+
+
+}
+
+let animals=[
+    {
+        "categoria":  "cats",
+        "name":"Cirmi",
+        "age":2,
+        "gender":"female",
+        "picture":"cirmi.jpg",
+        "des":"dddd",
+        "phone":"0255845225"
+        
+    }
+]
