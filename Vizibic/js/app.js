87e5bd0f4da4a60a2ca100a6061830c601080059
zamboilles21 <zamboilles@turr.hu>
2022-05-27 09:06:33 +0200

    09:06

---
+++ Vizibic/js/app.js
@@ -0,0 +1,12 @@
+let name=$('name').val(),
+    tipus=$('tipus').val(),
+    elvitel=$('elvitel').val(),
+    visszahozatal=$('visszahozatal').val(),
+    regBtn=document.querySelector('#RegBtn');
+
+$('#RegBtn').on('click',()=>{
+
+    
+
+
+})
