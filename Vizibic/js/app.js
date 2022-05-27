a9924560326f014fa1603948bd5c06e228a688b0
zamboilles21 <zamboilles@turr.hu>
2022-05-27 09:16:23 +0200

    lsknlask

--- Vizibic/js/app.js
+++ Vizibic/js/app.js
@@ -1,12 +1,11 @@
 let name=$('name').val(),
     tipus=$('tipus').val(),
     elvitel=$('elvitel').val(),
-    visszahozatal=$('visszahozatal').val(),
+    visszahozatal=$('visszahozatal').val();
     regBtn=document.querySelector('#RegBtn');
 
 $('#RegBtn').on('click',()=>{
+    localStorage.content = $('#container').html('Test');
 
-    
-
-
+    $('#container').html(localStorage.content);
 })
