f25accec61f94596438bb39e1b32c368488ab7bf
zamboilles21 <zamboilles@turr.hu>
2022-05-30 09:40:19 +0200

    fewltolt

--- Vizibic/js/app.js
+++ Vizibic/js/app.js
@@ -4,8 +4,34 @@ let name=$('name').val(),
     visszahozatal=$('visszahozatal').val();
     regBtn=document.querySelector('#RegBtn');
 
+let biciklik=[];
+
 $('#RegBtn').on('click',()=>{
     localStorage.content = $('#container').html('Test');
 
     $('#container').html(localStorage.content);
+    if (name.val=='' || tipus.val=='' || elvitel.val=='' || visszahozatal.value=='') {
+        alert('Hiányzó adat!')
+    }
+    else
+    {
+        bicikli={
+            
+            'name': name.val(),
+            'tipus':tipus.val(),
+            'elvitel':elvitel.val(),
+            'vissza':visszahozatal.val()
+            
+            
+        }
+        biciklik.push(bicikli);
+
+        localStorage.setItem('biciklik',JSON.stringify(biciklik));
+        alert('Felvéve!')
+    }
+
+
+    
 })
+
+document.querySelector('span').innerHTML=biciklik.length
